34d896ce86725a54cc80c57aa4f0fd6a3fd0cd61
gilomx <mgt.gilou@gmail.com>
2024-02-28 17:45:50 -0600

    Chnge plain to plane

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ const bosses =[
     {
         name: "Calamidad conjunta",
         character_name: "Hosco y Tosco",
-        is_plain: false,
+        is_plane: false,
         img: "/public/bosses/hoscoytosco.png"
     },
     {
         name: "Pánico botánico",
         character_name: "La pandilla raíz",
-        is_plain: false,
+        is_plane: false,
         img: "/public/bosses/pandillaraiz.png"
     },
     {
         name: "Treta en el légamo",
         character_name: "Goopy Le Grande",
-        is_plain: false,
+        is_plane: false,
         img: "/public/bosses/goopylegrande.png"
     },
     {
         name: "Dirigible temible",
         character_name: "Hilda Berg",
-        is_plain: true,
+        is_plane: true,
         img: "/public/bosses/hilda.png"
     },
     {
         name: "Furia floral",
         character_name: "Clavel de cagney",
-        is_plain: false,
+        is_plane: false,
         img: "/public/bosses/hilda.png"
     },
 ]
