16c5bb28380cc7b9bb3c49b21f8a4d4f98d93560
gilomx <mgt.gilou@gmail.com>
2024-02-28 17:27:15 -0600

    Adding bosses for island 1

---
+++ src/bosses.js
@@ -0,0 +1,33 @@
+const bosses =[
+    {
+        name: "Calamidad conjunta",
+        character_name: "Hosco y Tosco",
+        is_plain: false,
+        img: "/public/bosses/hoscoytosco.png"
+    },
+    {
+        name: "Pánico botánico",
+        character_name: "La pandilla raíz",
+        is_plain: false,
+        img: "/public/bosses/pandillaraiz.png"
+    },
+    {
+        name: "Treta en el légamo",
+        character_name: "Goopy Le Grande",
+        is_plain: false,
+        img: "/public/bosses/goopylegrande.png"
+    },
+    {
+        name: "Dirigible temible",
+        character_name: "Hilda Berg",
+        is_plain: true,
+        img: "/public/bosses/hilda.png"
+    },
+    {
+        name: "Furia floral",
+        character_name: "Clavel de cagney",
+        is_plain: true,
+        img: "/public/bosses/hilda.png"
+    },
+
+]
